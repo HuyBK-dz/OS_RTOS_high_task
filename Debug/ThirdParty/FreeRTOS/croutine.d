30250232af233308d8116c3accb455f49c481c18
unknown <huybk190801@gmail.com>
2023-03-30 20:52:00 +0700

    add file

---
+++ Debug/ThirdParty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+ThirdParty/FreeRTOS/croutine.o: ../ThirdParty/FreeRTOS/croutine.c \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/portable.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/list.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/croutine.h
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/portable.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/list.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/croutine.h:
---
+++ Debug/ThirdParty/FreeRTOS/timers.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/timers.o: ../ThirdParty/FreeRTOS/timers.c \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/portable.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/list.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/queue.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h \
+ D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/timers.h
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/portable.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/list.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/queue.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/task.h:
+D:/20221/He_dieu_hanh_code/Full_code/Desktop/RTOS_Workspace(fibonanci)/ThirdParty/FreeRTOS/include/timers.h:
